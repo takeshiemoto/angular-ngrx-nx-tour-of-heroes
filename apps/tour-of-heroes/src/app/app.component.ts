8a1cc31640da399b3dbd108ecea96a82ea24a3c6
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-03-08 21:11:43 +0900

    initial commit

---
+++ apps/tour-of-heroes/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'angular-ngrx-nx-tour-of-heroes-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'tour-of-heroes';
+}
